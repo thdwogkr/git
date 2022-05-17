6a25d41c3829fb0117e1ec8ab37f2ea894ef3f72
thdwogkr <sjhsjh1990@gmail.com>
2022-05-17 11:20:08 +0900

    수정버전

--- oop/src/Test.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class Test {
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        int[] reverse_arr = reverseArr(Arrays.copyOfRange(arr,0,arr.length-1));
-    }
-}
