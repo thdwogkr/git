ae4b8f4bd423e6f93a28402b019867aaab74950a
thdwogkr <sjhsjh1990@gmail.com>
2022-05-19 16:16:02 +0900

    0519_ex7_v1

--- ex7/src/Fib.java
+++ ex7/src/Fib.java
@@ -1,18 +1,44 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class Fib {
-
     public static void main(String[] args) {
+        System.out.println("[안내]피보나치 수열 프로그램 시작.");
+        Input a = new Input();
+        a.what();
+        a.cal();
+        System.out.println("[안내]프로그램을 종료합니다.");
+    }
+}
 
-        Fibo() ex = new Fibo();
-        ex = 10;
-        System.out.println(ex);
 
+class Input {
+
+    int num;
+    int[] sum;
+
+    void what() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("원하는 수열의 개수를 입력해 주세요 : ");
+        num = sc.nextInt();
+        sc.close();
+    }
+
+    void cal() {
+        System.out.println("[피보나치 수열 출력]");
+        sum = new int[num];
+        sum[0] = 1;
+        sum[1] = 1;
+        for(int i = 2 ; i < num ; i++ ){
+            sum[i] = sum[i-1] + sum[i-2];
+        }
+
+        for(int j = 0 ; j < num ; j ++) {
+            System.out.println(sum[j]);
+        }
 
 
     }
 
-
-    public static int Fibo(int num) {
-        if (num <= 2) return 1;
-        Fibo(num) = Fibo(num - 1) + Fibo(num - 2);
-    }
 }
